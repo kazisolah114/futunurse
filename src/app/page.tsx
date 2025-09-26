c5b3122f405c1a5f29d77a89654c759e9da54444
Kazi Solayman <kazisolayman123@gmail.com>
2025-09-26 15:15:46 -0500

    Added hero section to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Header from "@/components/header/header";
+import Header from "@/components/landing/header/header";
+import Hero from "@/components/landing/hero/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        
+        <Hero />
       </main>
     </>
   );
--- src/components/landing/header/header.tsx
+++ src/components/landing/header/header.tsx
@@ -4,7 +4,7 @@ import Navbar from './navbar';
 
 const Header = () => {
     return (
-        <header className='header flex justify-between items-center p-4'>
+        <header className='header h-16 px-28 flex justify-between items-center bg-white border-b'>
             <Link href={"/"}><h2 className='font-bold text-3xl text-teal-600'>FutuNurse</h2></Link>
             <Navbar />
         </header>

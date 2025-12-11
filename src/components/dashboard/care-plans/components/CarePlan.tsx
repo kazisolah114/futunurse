235ced1d76c75b50223e5dfc79d0cd09d7efe081
Kazi Solayman <kazisolayman123@gmail.com>
2025-12-11 10:41:49 -0600

    Updated care plan item UI

--- src/components/dashboard/care-plans/components/CarePlan.tsx
+++ src/components/dashboard/care-plans/components/CarePlan.tsx
@@ -12,6 +12,7 @@ interface CarePlanProps {
 }
 
 const CarePlan = ({ carePlan }: CarePlanProps) => {
+    console.log(carePlan)
     const pathname = usePathname();
     const { _id, patient, createdAt, updatedAt } = carePlan || {};
     const handleDeletePlan = async () => {
@@ -28,25 +29,37 @@ const CarePlan = ({ carePlan }: CarePlanProps) => {
     };
     return (
         <div className='w-full bg-white border border-gray-200/30 hover:border-gray-200/50 duration-200 p-5 rounded-md'>
-            <div className='flex items-center gap-3'>
-                <h2 className='text-xl font-bold text-gray-800'>{patient.name}</h2> <span className='px-2 py-0.5 border rounded-full text-xs'>{patient.specialty}</span>
+            <div className='sm:flex items-center justify-between gap-3'>
+                <h2 className='text-xl font-bold text-gray-800'>
+                    {patient.name}
+                </h2>
+                <div className='space-x-2 max-sm:mt-3'>
+                    <span className='font-medium px-2 py-0.5 border rounded-full text-xs'>
+                        {patient.specialty}
+                    </span>
+                    <span className='font-medium px-2 py-0.5 border border-blue-200 bg-blue-500/10 text-blue-600 rounded-full text-xs'>
+                        {carePlan.diagnoses?.length} Diagnoses
+                    </span>
+                </div>
             </div>
             <p className='text-gray-800 my-2'>{patient.primaryDiagnoses}</p>
             <div className='flex sm:items-center gap-8 max-sm:flex-col max-sm:gap-2'>
                 <p className='text-sm text-gray-500 flex items-center gap-2'><Calendar size={16} /> Created {new Date(createdAt).toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}</p>
                 <p className='text-sm text-gray-500 flex items-center gap-2'><Clock size={16} /> Updated {new Date(updatedAt).toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}</p>
             </div>
-            <div className='space-x-2 mt-6'>
+            <div className='space-x-2 max-sm:space-x-4 mt-6'>
                 <Link href={{
                     pathname: `${pathname}/${_id}`,
                     query: {
                         patient: patient.name?.toLowerCase(),
                         specialty: patient.specialty?.toLowerCase()
                     }
-                }}><Button><Stethoscope size={18} /> View Details</Button></Link>
-                <Button variant={'outline'}><PencilLine size={18} /> Edit Plan</Button>
-                <Button className='bg-transparent text-gray-700 hover:text-yellow-500 hover:bg-yellow-500/20 max-sm:w-1'><Star size={18} /></Button>
-                <Button onClick={handleDeletePlan} className='bg-transparent text-gray-700 hover:text-red-500 hover:bg-red-500/20 max-sm:w-1'><Trash size={18} /></Button>
+                }}>
+                    <Button className='max-sm:w-52'><Stethoscope size={18} /> View Details</Button>
+                </Link>
+                <Button variant={'outline'} className='max-sm:hidden'><PencilLine size={18} /> Edit Plan</Button>
+                <Button className='bg-transparent text-gray-700 hover:text-yellow-500 hover:bg-yellow-500/20 max-sm:w-2'><Star size={18} /></Button>
+                <Button onClick={handleDeletePlan} className='bg-transparent text-gray-700 hover:text-red-500 hover:bg-red-500/20 max-sm:w-2'><Trash size={18} /></Button>
             </div>
         </div>
     );

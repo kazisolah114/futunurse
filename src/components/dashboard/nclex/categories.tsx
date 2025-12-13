ba28f42a4cb7587c578341b5110bfaa946f29dc8
Kazi Solayman <kazisolayman123@gmail.com>
2025-12-12 23:51:26 -0600

    Fixed categories responsiveness issues

--- src/components/dashboard/nclex/categories.tsx
+++ src/components/dashboard/nclex/categories.tsx
@@ -38,31 +38,31 @@ export const Categories = () => {
                 {
                     nclexCategories.map((category, idx) => <div key={idx} className='bg-white border border-gray-200/30 rounded-lg p-5'>
                         <div className='flex items-center gap-4 mb-5'>
-                            <div className='bg-teal-600/20 text-teal-600 w-11 h-11 rounded-lg flex items-center justify-center'>
+                            <div className='max-sm:hidden bg-teal-600/20 text-teal-600 w-11 h-11 rounded-lg flex items-center justify-center'>
                                 <Stethoscope size={21} />
                             </div>
                             <div>
-                                <h2 className='font-semibold text-lg text-gray-900 mb-0.5'>{category.name}</h2>
+                                <h2 className='font-semibold max-sm:font-bold max-sm:text-gray-800 text-lg text-gray-900 mb-0.5'>{category.name}</h2>
                                 <p className='text-gray-600 text-sm normal-case'>{category.subcategories.slice(0, 2).join(", ")}</p>
                             </div>
                         </div>
                         <div>
                             <div className='flex items-center justify-between'>
                                 <p className='text-gray-600'>Progress</p>
-                                <p className={`text-sm font-medium w-16 py-0.5 text-center rounded-full ${100 >= 70 ? 'bg-green-500/20 text-green-700' : 50 >= 40 ? 'bg-yellow-500/20 text-yellow-700' : 'bg-red-100 text-red-500'}`}>50%</p>
+                                <p className={`text-sm sm:font-medium w-16 max-sm:w-14 py-0.5 text-center rounded-full ${100 >= 70 ? 'bg-green-500/20 text-green-700' : 50 >= 40 ? 'bg-yellow-500/20 text-yellow-700' : 'bg-red-100 text-red-500'}`}>50%</p>
                             </div>
                             <Progress value={50} className='my-2' />
                             <p className='text-gray-500'>245 questions completed</p>
                         </div>
                         <div className='mt-5'>
                             <h3 className='uppercase text-sm font-semibold text-gray-600 mb-2'>Key topics</h3>
-                            <ul className='flex items-center gap-2'>
+                            <ul className='flex items-center gap-2 flex-wrap'>
                                 {category.subcategories.map((i, idx) => <li key={idx} className='border border-teal-600 text-teal-600 py-0.5 px-2 rounded-full text-xs'>{i}</li>)}
                             </ul>
                         </div>
                         <div className='mt-5 flex gap-4'>
-                            <Button size={'lg'} className='flex-1'><PlayIcon /> Start Session</Button>
-                            <Button size={'lg'} variant={'secondary'} className='w-44'><BookA /> Study Guide</Button>
+                            <Button size={'lg'} className='sm:flex-1 w-6/12'><PlayIcon /> Start Session</Button>
+                            <Button size={'lg'} variant={'secondary'} className='w-44 max-sm:w-fit'><BookA /> Study Guide</Button>
                         </div>
                     </div>
                     )

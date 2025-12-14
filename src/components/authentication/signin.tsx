a262fe6c6388d515921012770521de7049321288
Kazi Solayman <kazisolayman123@gmail.com>
2025-12-14 16:18:29 -0600

    Updated auth popup for sign in and sign up

--- src/components/authentication/signin.tsx
+++ src/components/authentication/signin.tsx
@@ -6,6 +6,7 @@ import { signIn } from 'next-auth/react';
 import GoogleIcon from "../../../public/icons/google.png";
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { Eye, LucideEyeOff } from 'lucide-react';
 
 interface SignInProps {
     signUpInstead: () => void;
@@ -24,6 +25,7 @@ const SignIn = ({ signUpInstead, onClose }: SignInProps) => {
         email: '',
         password: ''
     });
+    const [showPasswor, setShowPassword] = useState<boolean>(false);
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setLoading(true);
@@ -49,27 +51,33 @@ const SignIn = ({ signUpInstead, onClose }: SignInProps) => {
     }
     return (
         <div>
-            <div className='flex items-center justify-center flex-col'>
+            <div className='flex items-center justify-center flex-col text-center'>
                 <h5 className='font-bold text-lg text-gray-900 uppercase'>Welcome Back</h5>
                 <p className='text-gray-700 text-sm'>Sign in to your account to continue your nursing journey</p>
             </div>
-            <Button onClick={() => signIn("google", { callbackUrl: "/dashboard" })} variant={'outline'} className='w-full my-6'><Image src={GoogleIcon || null} alt={"google-icon"} width={30} height={30} /> Continue with Google</Button>
+            <Button onClick={() => signIn("google", { callbackUrl: "/dashboard" })} size={'lg'} variant={'secondary'} className='w-full my-6 rounded-full'>
+                <Image src={GoogleIcon || null} alt={"google-icon"} priority width={30} height={30} />
+                Sign in with Google
+            </Button>
             <form onSubmit={handleSubmit} className=' space-y-4'>
-                <div className='space-y-2'>
+                <div className='space-y-2.5'>
                     <Label htmlFor='email'>Email</Label>
                     <Input type='email' placeholder='student@nursing.edu' required
                         value={formData.email}
                         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                     />
                 </div>
-                <div className='space-y-2'>
+                <div className='space-y-2.5 relative'>
                     <Label htmlFor='password'>Password</Label>
-                    <Input type='password' placeholder='Enter password' required
+                    <Input type={showPasswor ? 'text' : 'password'} placeholder='Enter password' required
                         value={formData.password}
                         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                     />
+                    <Button onClick={() => setShowPassword(!showPasswor)} variant={'ghost'} size={'icon'} className='absolute top-6.5 right-0.5' type='button'>
+                        {showPasswor ? <Eye /> : <LucideEyeOff /> }
+                    </Button>
                 </div>
-                <Button size={'lg'} className='w-full mt-4'>{loading ? 'Loading...' : 'Sign In'}</Button>
+                <Button type='submit' size={'lg'} className='w-full mt-4 rounded-full'>{loading ? 'Loading...' : 'Sign In'}</Button>
             </form>
             <p className='text-gray-700 text-center text-sm mt-3'>Don&apos; have an account? <Button variant={'link'} onClick={signUpInstead}>Sign Up</Button></p>
         </div>
--- src/components/authentication/signup.tsx
+++ src/components/authentication/signup.tsx
@@ -4,6 +4,8 @@ import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import GoogleIcon from "../../../public/icons/google.png";
 
 interface SignUpProps {
     signInInstead: () => void;
@@ -67,40 +69,46 @@ const SignUp = ({ signInInstead, onClose }: SignUpProps) => {
     }
     return (
         <div>
-            <div className='flex items-center justify-center flex-col'>
+            <div className='flex items-center justify-center flex-col text-center'>
                 <h5 className='font-bold text-lg text-gray-900 uppercase'>Sign Up Now</h5>
                 <p className='text-gray-700 text-sm'>Sign up to create your account to continue your nursing journey</p>
             </div>
-            <form onSubmit={handleSubmit} className='mt-6 space-y-4'>
-                <div className='space-y-2'>
+
+            <Button onClick={() => signIn("google", { callbackUrl: "/dashboard" })} size={'lg'} variant={'secondary'} className='w-full my-6 rounded-full'>
+                <Image src={GoogleIcon || null} alt={"google-icon"} priority width={30} height={30} />
+                Continue with Google
+            </Button>
+
+            <form onSubmit={handleSubmit} className='space-y-4'>
+                <div className='space-y-2.5'>
                     <Label htmlFor='name'>Full name</Label>
                     <Input type='text' placeholder='e,g. Donald Trump' required
                         value={formData.fullName}
                         onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                     />
                 </div>
-                <div className='space-y-2'>
+                <div className='space-y-2.5'>
                     <Label htmlFor='email'>Email</Label>
                     <Input type='email' placeholder='student@nursing.edu' required
                         value={formData.email}
                         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                     />
                 </div>
-                <div className='space-y-2'>
+                <div className='space-y-2.5'>
                     <Label htmlFor='password'>Password</Label>
                     <Input type='password' placeholder='Enter password' required
                         value={formData.password}
                         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                     />
                 </div>
-                <div className='space-y-2'>
+                <div className='space-y-2.5'>
                     <Label htmlFor='cpassword'>Confirm password</Label>
                     <Input type='password' placeholder='Confirm password' required
                         value={formData.confirmPassword}
                         onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
                     />
                 </div>
-                <Button size={'lg'} className='w-full mt-4'>{loading ? 'Loading...' : 'Sign Up'}</Button>
+                <Button size={'lg'} className='w-full mt-4 rounded-full'>{loading ? 'Loading...' : 'Sign Up'}</Button>
             </form>
             <p className='text-gray-700 text-center text-sm mt-3'>Already have an account? <Button variant={'link'} onClick={signInInstead}>Sign In</Button></p>
         </div>
